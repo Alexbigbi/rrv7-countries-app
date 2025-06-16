45f0a7245b36b7576612f8b4d71d8640b338058d
Alexbigbi <122051653+Alexbigbi@users.noreply.github.com>
2025-06-15 19:22:18 -0600

    Add info and style About page and update README
    This commit adds info to our about page with the technologies used,
    the goal of the web application and credits to PedroTech for the
    tutorial.
    
    The page was also styled, and the README was updated to contain the
    same info in the about page.
    
    TODO: Deploy the page using fly.

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,3 +1,25 @@
 export default function About() {
-    return <div>About Page</div>
+    return (
+        <div className="py-16 bg-white">
+            <div className="container mx-auto px-4 max-w-4xl space-y-4">
+                <h1 className="text-4xl font-extrabold text-gray-900 mb-8 text-center"> About REST<span className="text-indigo-600">Explorer </span> </h1>
+                <p className="text-xl text-gray-700 leading-relaxed">
+                    This website uses the <span className="text-indigo-600 font-bold"> REST Countries API </span>
+                    to display comprehensive information about countries from around the world.
+                </p>
+                <p className="text-lg text-gray-700 leading-relaxed">
+                    This is part of a tutorial by <span className="text-indigo-600 font-bold"> PedroTech </span>
+                    that makes use of ReactJS, React Router v7, TailwindCSS and TypeScript. 
+                </p>
+                <p className="text-lg text-gray-700 leading-relaxed">
+                    Explore the many countries of our world in our interactive page and learn their capitals, 
+                    population, flags, official names and much more.
+                </p>
+                <p className="text-lg text-gray-700 leading-relaxed">
+                    Our goal was to learn RRV7, TailwindCSS, ReactJS and TypeScript by building a highly responsive, modern and feature-rich web application.
+                </p>
+            </div>
+        </div>
+    )
+
 }
