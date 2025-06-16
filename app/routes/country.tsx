07a470f8f75d5eb6fc74ee6ea96b53eb9cef0ce6
Alexbigbi <122051653+Alexbigbi@users.noreply.github.com>
2025-06-15 18:51:31 -0600

    Style country page

--- app/routes/country.tsx
+++ app/routes/country.tsx
@@ -18,36 +18,37 @@ export default function Country({ loaderData }: Route.ComponentProps) {
         capital: loaderData[0]?.capital || "N/A",
         population: loaderData[0]?.population || "N/A",
         flagUrl: loaderData[0]?.flags?.png|| "",
-        
+    };
 
-    }
     return (
-    <div>
-        <div>
-            <h2> {country.name} </h2>
-            <div>
+    <div className="p-6 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold text-gray-900"> {country.name} </h2>
+            <div className="space-y-2 text-gray-700">
                 <p> 
-                    <span> Official Name:</span> {country.officialName}
+                    <span className="font-semibold"> Official Name:</span> {country.officialName}
                 </p>
                 <p> 
-                    <span> Region:</span> {country.region}
+                    <span className="font-semibold"> Region:</span> {country.region}
                 </p>
                 <p> 
-                    <span> Subregion:</span> {country.subregion}
+                    <span className="font-semibold"> Subregion:</span> {country.subregion}
                 </p>
                 <p> 
-                    <span> Capital:</span> {country.capital}
+                    <span className="font-semibold"> Capital:</span> {country.capital}
                 </p>
                 <p> 
-                    <span> Population:</span> {country.population}
+                    <span className="font-semibold"> Population:</span> {country.population.toLocaleString()}
                 </p>
             </div>
         </div>
         {country.flagUrl && (
-            <div>
-                <img src={country.flagUrl} />
-            </div>
+        <div className="flex justify-center items-center">
+            <img src={country.flagUrl} 
+            className="w-56 h-auto border rounded shadow-lg"
+            />
+        </div>
         )}
     </div>
-    )
+    );
 }
